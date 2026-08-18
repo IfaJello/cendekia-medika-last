8fc30e938a2ece6502921ddef8d55ec880463f7a
IfaJello <ifa.imanda.as@gmail.com>
2026-08-18 08:02:40 +0000

    feat: add safety and psychosocial SDKI categories

--- src/data/nursing-standard/sdkid/index.js
+++ src/data/nursing-standard/sdkid/index.js
@@ -1,10 +1,18 @@
 import { physiologicalDiagnoses } from "./physiological";
 import { circulationDiagnoses } from "./circulation";
 import { nutritionDiagnoses } from "./nutrition";
+import { eliminationDiagnoses } from "./elimination";
+import { activityDiagnoses } from "./activity";
+import { safetyDiagnoses } from "./safety";
+import { psychosocialDiagnoses } from "./psychosocial";
 
 
 export const sdkid = [
   ...physiologicalDiagnoses,
   ...circulationDiagnoses,
-  ...nutritionDiagnoses
+  ...nutritionDiagnoses,
+  ...eliminationDiagnoses,
+  ...activityDiagnoses,
+  ...safetyDiagnoses,
+  ...psychosocialDiagnoses
 ];
---
+++ src/data/nursing-standard/sdkid/psychosocial.js
@@ -0,0 +1,101 @@
+export const psychosocialDiagnoses = [
+
+  {
+    code: "D.0080",
+
+    name: "Ansietas",
+
+    domain: "Psikologis",
+
+    class: "Respons Emosional",
+
+    definition:
+      "Kondisi emosi dan pengalaman subjektif terhadap objek yang tidak jelas.",
+
+    relatedFactors: [
+      "Krisis situasional",
+      "Ancaman terhadap kesehatan",
+      "Kurang informasi"
+    ],
+
+    definingCharacteristics: [
+      "Merasa khawatir",
+      "Gelisah",
+      "Sulit berkonsentrasi"
+    ],
+
+    slki: [
+      "L.09093"
+    ],
+
+    siki: [
+      "I.09314"
+    ]
+  },
+
+
+  {
+    code: "D.0096",
+
+    name: "Koping Tidak Efektif",
+
+    domain: "Psikologis",
+
+    class: "Respons Koping",
+
+    definition:
+      "Ketidakmampuan menilai stresor secara adekuat dan memilih respons yang sesuai.",
+
+    relatedFactors: [
+      "Ketidakmampuan menghadapi masalah",
+      "Kurang dukungan sosial"
+    ],
+
+    definingCharacteristics: [
+      "Mengungkapkan ketidakmampuan menghadapi masalah",
+      "Perilaku tidak adaptif"
+    ],
+
+    slki: [
+      "L.09086"
+    ],
+
+    siki: [
+      "I.09265"
+    ]
+  },
+
+
+  {
+    code: "D.0084",
+
+    name: "Gangguan Citra Tubuh",
+
+    domain: "Psikologis",
+
+    class: "Konsep Diri",
+
+    definition:
+      "Perubahan persepsi tentang penampilan, struktur, atau fungsi tubuh.",
+
+    relatedFactors: [
+      "Perubahan struktur tubuh",
+      "Penyakit",
+      "Tindakan medis"
+    ],
+
+    definingCharacteristics: [
+      "Mengungkapkan perubahan tubuh",
+      "Menolak melihat bagian tubuh tertentu"
+    ],
+
+    slki: [
+      "L.09067"
+    ],
+
+    siki: [
+      "I.09305"
+    ]
+  }
+
+];
